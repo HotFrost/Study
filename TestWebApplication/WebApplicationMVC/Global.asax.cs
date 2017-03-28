1cc04b8d3fcd67b15fce15f56fe857b5e25e2eca
HotFrost <hotfrost@gmail.com>
2017-03-28 11:56:11 -0400

    here is the second change

--- TestWebApplication/WebApplicationMVC/Global.asax.cs
+++ TestWebApplication/WebApplicationMVC/Global.asax.cs
@@ -17,7 +17,7 @@ namespace WebApplicationMVC
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
-            BundleConfig.RegisterBundles(BundleTable.Bundles);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);//here is the second change
         }
     }
 }
