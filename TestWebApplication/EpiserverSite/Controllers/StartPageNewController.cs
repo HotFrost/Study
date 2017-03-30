9ddbf9a8680a00f32e618d3eda1af107dd5b3cb1
HotFrost <alex.tsepota@karmaloop.com>
2017-03-30 00:23:09 -0400

    added projects for EPI server

---
+++ TestWebApplication/EpiserverSite/Controllers/StartPageNewController.cs
@@ -0,0 +1,15 @@
+﻿using EPiServer.Web.Mvc;
+using EpiserverSite.Models.Pages;
+using System.Web.Mvc;
+
+namespace EpiserverSite.Controllers
+{
+    public class StartPageNewController : PageController<StartPageNew>
+    {
+        // GET: StartPageNew
+        public ActionResult Index(StartPageNew currentPage)
+        {
+            return View();
+        }
+    }
+}
---
+++ TestWebApplication/EpiserverSiteEmpty/Models/Pages/StartPage.cs
@@ -0,0 +1,18 @@
+﻿using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using System.ComponentModel.DataAnnotations;
+
+namespace EpiserverSiteEmpty.Models.Pages
+{
+    [ContentType(DisplayName = "StartPage", GUID = "e2c7d9c9-4a87-4ecd-b99f-b5b315d4eab4", Description = "")]
+    public class StartPage : PageData
+    {
+        [CultureSpecific]
+        [Display(Name = "Main body",Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+        GroupName = SystemTabNames.Content,Order = 1)]
+
+        public virtual XhtmlString MainBody { get; set; }
+
+    }
+}
