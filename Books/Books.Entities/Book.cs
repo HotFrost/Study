c00078dbe8bf6d265f9af1b5ed21a11503c409ef
Alexey Tsepota <atsepota@air-worldwide.com>
2017-04-10 11:12:08 -0400

    added Books solution

---
+++ Books/Books.Entities/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Build.Framework;
+
+namespace Books.Entities
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+
+        public Genre Category { get; set; }
+    }
+}
---
+++ Books/Books.Web/AceDataContext/BooksDb.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using Books.Entities;
+
+namespace Books.Web.AceDataContext
+{
+    public class BooksDb:DbContext          
+    {
+        public BooksDb(): base("DefaultConnection")
+        {
+            Database.Log = sql => Debug.Write(sql);
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
