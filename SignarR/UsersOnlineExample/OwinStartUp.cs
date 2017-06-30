8223f730d812fadd9f1839a036cafc02488aef94
Alexey Tsepota <atsepota@air-worldwide.com>
2017-06-30 14:31:55 -0400

    added SignalR project

---
+++ SignarR/UsersOnlineExample/OwinStartUp.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(UsersOnlineExample.OwinStartUp))]
+
+namespace UsersOnlineExample
+{
+    public class OwinStartUp
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+        }
+    }
+}
+
